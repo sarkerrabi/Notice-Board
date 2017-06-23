735dd2d02050475ae16ee032d3728ba9482b7503
sarkerrabi <sarkerrabi16@gmail.com>
2017-06-24 00:23:40 +0600

    added try catches in main.py file for avoiding error from down websites. change the index.html file

--- main.py
+++ main.py
@@ -6,35 +6,62 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/nlist')
+def nlist():
     t10n = Top10Notices()
     noticList = t10n.nsu_top10_notice()
     uniname =  t10n.uniList()
     listNo = list(range(1,len(noticList[0])))
-    return render_template('index.html',notices =zip(noticList[0],listNo,noticList[1]),uniname =uniname)
+    return render_template('nlist.html',notices =zip(noticList[0],listNo,noticList[1]),uniname =uniname)
 
-@app.route('/nlist')
-def nlist():
+@app.route('/')
+def index():
     top10 = Top10Notices()
     unilist = top10.uniList()
 
     # lengths = top10.selected_unis_length()
     # # unis = top10.selected_unis()
     # # uni_zip = zip(lengths,unis)
-    nsu_zip = zip(list(range(1,len(top10.nsu_top10_notice()[0]))),top10.nsu_top10_notice()[0],top10.nsu_top10_notice()[1])
-    aiub_zip = zip(list(range(1,len(top10.aiub_top10_notice()[0]))),top10.aiub_top10_notice()[0],top10.aiub_top10_notice()[1])
-    bracu_zip = zip(list(range(1,len(top10.bracu_top10_notice()[0]))),top10.bracu_top10_notice()[0],top10.bracu_top10_notice()[1])
-    ewu_zip = zip(list(range(1,len(top10.ewu_top10_notice()[0]))),top10.ewu_top10_notice()[0],top10.ewu_top10_notice()[1])
-    iub_zip = zip(list(range(1,len(top10.iub_top10_notice()[0]))),top10.iub_top10_notice()[0],top10.iub_top10_notice()[1])
-    iubat_zip = zip(list(range(1,len(top10.iubat_top10_notice()[0]))),top10.iubat_top10_notice()[0],top10.iubat_top10_notice()[1])
-    uiu_zip = zip(list(range(1,len(top10.uiu_top10_notice()[0]))),top10.uiu_top10_notice()[0],top10.uiu_top10_notice()[1])
-    seu_zip = zip(list(range(1,len(top10.seu_top10_notice()[0]))),top10.seu_top10_notice()[0],top10.seu_top10_notice()[1])
+    try:
+        nsu_zip = zip(list(range(1,len(top10.nsu_top10_notice()[0]))),top10.nsu_top10_notice()[0],top10.nsu_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+
+    try:
+        aiub_zip = zip(list(range(1,len(top10.aiub_top10_notice()[0]))),top10.aiub_top10_notice()[0],top10.aiub_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+    try:
+        bracu_zip = zip(list(range(1,len(top10.bracu_top10_notice()[0]))),top10.bracu_top10_notice()[0],top10.bracu_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+    try:
+        ewu_zip = zip(list(range(1,len(top10.ewu_top10_notice()[0]))),top10.ewu_top10_notice()[0],top10.ewu_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+    try:
+        iub_zip = zip(list(range(1,len(top10.iub_top10_notice()[0]))),top10.iub_top10_notice()[0],top10.iub_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+    try:
+        iubat_zip = zip(list(range(1,len(top10.iubat_top10_notice()[0]))),top10.iubat_top10_notice()[0],top10.iubat_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+
+    try:
+        uiu_zip = zip(list(range(1,len(top10.uiu_top10_notice()[0]))),top10.uiu_top10_notice()[0],top10.uiu_top10_notice()[1])
+    except ValueError:
+        print("sorry")
+
+    try:
+        seu_zip = zip(list(range(1,len(top10.seu_top10_notice()[0]))),top10.seu_top10_notice()[0],top10.seu_top10_notice()[1])
+    except ValueError:
+        print("sorry")
 
 
 
 
-    return render_template('nlist.html',bracu_zip=bracu_zip,unilist=unilist,nsu_zip=nsu_zip,aiub_zip = aiub_zip,ewu_zip=ewu_zip,iub_zip=iub_zip,iubat_zip=iubat_zip,uiu_zip=uiu_zip,seu_zip=seu_zip)
+    return render_template('index.html',bracu_zip=bracu_zip,unilist=unilist,nsu_zip=nsu_zip,aiub_zip = aiub_zip,ewu_zip=ewu_zip,iub_zip=iub_zip,iubat_zip=iubat_zip,uiu_zip=uiu_zip,seu_zip=seu_zip)
 
 
 
